e5edebd9ea56d8a9bdff242ec326e0785af054b9
king-ak <akdavey@email.wm.edu>
2023-06-20 16:53:22 -0400

    capture changes

--- scripts/rbac-feature/deploy-arc-enabled-k8s-feature.sh
+++ scripts/rbac-feature/deploy-arc-enabled-k8s-feature.sh
@@ -1,8 +1,6 @@
 cluster_name=$1
 rg_name=$2
-extension_name="az-policy-ext" # Name of the extension
-extension_type="Microsoft.PolicyInsights" # Type of the extension
-extension_desc="Azure Policy"
+feature_name="azure-rbac"
 
 rbac_config_file_path="tmp/rbac-setup-config.json"
 
@@ -17,14 +15,14 @@ az extension update --name k8s-extension
 # Enable feature
 server_app_id=$(jq -r .serverAppID $rbac_config_file_path)
 server_app_secret=$(jq -r .serverAppSecret $rbac_config_file_path)
-echo "Creating extension instance for ${extension_desc} ..."
-az connectedk8s enable-features --cluster-name $cluster_name \
+echo "Enabling feature ${feature_name} ..."
+az connectedk8s enable-features --name $cluster_name \
                                 --resource-group $rg_name \
-                                --features azure-rbac \
+                                --features $feature_name \
                                 --app-id "${server_app_id}" \
                                 --app-secret "${server_app_secret}"
 
 # Try Cluster API steps from docs
 # Copy the guard secret that contains authentication and authorization webhook configuration files
-kubectl get secret azure-arc-guard-manifests -n kube-system \
-                                             -o yaml > tmp/azure-arc-guard-manifests.yaml
+# kubectl get secret azure-arc-guard-manifests -n kube-system \
+#                                              -o yaml > tmp/azure-arc-guard-manifests.yaml
